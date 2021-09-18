755a27db6157bf478507af146e03a2aa6ca1ffbf
krishnam@sharpcodesolutions.net <krishnam@sharpcodesolutions.net>
2021-09-18 16:26:45 +0530

    string editable

--- src/components/DataTypes/StringComponent.js
+++ src/components/DataTypes/StringComponent.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 class StringComponent extends Component {
     constructor(props) {
-        super(props);    
+        super(props);  
+        this.state = {
+            val : props.v
+        }  
+    }
+
+    handleChange(e){
+        this.setState({'val' : [e.target.value]})
     }
 
     render(){
         return (
-            <div>&quot; {this.props.k} &quot; : &quot; {this.props.v} &quot;</div>
+            <div>&quot; {this.props.k} &quot; : 
+            <input type = "text" value = {this.state.val} onChange = {(e) => this.handleChange(e)}></input> 
+            </div>
         );
     }
 }
